a3ae8e46bf7685bae63addb70a1821d2e02eb94d
Arthur <starwords2013@gmail.com>
2018-12-21 10:56:49 +0800

    add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @authors Arthur Guan (starwords2013@gmail.com)
+ * @date    2018-12-21 10:55:03
+ * @version 1.0
+ */
+
+import java.lang.ArrayList;
+
+public static void main(String[] args) {
+	System.out.println("Hello world!");
+}
